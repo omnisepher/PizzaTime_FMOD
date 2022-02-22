5837eb500e151cdc8d2a4b0bd1e2122fd576524a
Sepher <misunra8@gmail.com>
2022-02-22 18:47:07 +0300

    L 03 Continuous Switch

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -10,6 +10,8 @@ public class AudioManager : MonoBehaviour
 
     [SerializeField] private FMODUnity.EventReference _themeSong;
     private FMOD.Studio.EventInstance themeSong;
+    [SerializeField] private FMODUnity.EventReference _clockTick;
+    private FMOD.Studio.EventInstance clockTick;
 
     private bool isPlaying = false;
 
@@ -25,13 +27,35 @@ public class AudioManager : MonoBehaviour
         themeSong = FMODUnity.RuntimeManager.CreateInstance(_themeSong);
         // themeSong = FMODUnity.RuntimeManager.CreateInstance(FMOD.GUID.Parse("{609170fc-98f4-4d9d-b61d-b7680674cc68}")); //{609170fc-98f4-4d9d-b61d-b7680674cc68}
         // themeSong = FMODUnity.RuntimeManager.CreateInstance("event:/Menu/Menu 1");
+        clockTick = FMODUnity.RuntimeManager.CreateInstance(_clockTick);
 
         if (themeSong.isValid())
         {
             themeSong.start();
             themeSong.setVolume(1.0f);
             isPlaying = true;
-            Debug.Log("Started!");
+        }
+
+        if (clockTick.isValid())
+        {
+            clockTick.start();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            PauseResume();
+        }
+
+        if (GameManager.timer < 5.0f)
+        {
+            clockTick.setParameterByName("Volume", 1);
+        }
+        else
+        {
+            clockTick.setParameterByName("Volume", 0);
         }
     }
 
